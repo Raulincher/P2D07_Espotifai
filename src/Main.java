010ca26a0622e3b09d3ebba15e54da13080f2050
Raulincher <raulincher@gmail.com>
2023-03-20 15:20:08 +0100

    testing in main 2.0

--- src/Main.java
+++ src/Main.java
@@ -20,11 +20,16 @@ public class Main {
         Song song1 = new Song("the stage", "HeavyMetal", "The stage", "Avenged Sevenfold");
         User user1 = new User("raulincher", "hola123", "raulincher@gmail.com");
 
+        Song song2 = new Song("mama", "HeavyMetal", "The black parade", "My chemical romance");
+        User user2 = new User("pepe", "1234567", "pepe@gmail.com");
 
         songDao.SaveSong(song1);
         userDao.register(user1);
+
         if(userDao.userExists(user1)){
-            System.out.println("Users exists");
+            System.out.println("User 1 exists");
+            songDao.SaveSong(song2);
+            userDao.register(user2);
         }
 
 
