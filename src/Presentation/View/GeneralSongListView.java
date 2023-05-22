949fd7471aade9d9eab793043418bf18efd126e4
aidenx10 <albert.lligadas@gmail.com>
2023-05-22 20:12:51 +0200

    Més JavaDoc

--- src/Presentation/View/GeneralSongListView.java
+++ src/Presentation/View/GeneralSongListView.java
@@ -17,17 +17,31 @@ import static javax.swing.BorderFactory.createEmptyBorder;
 
 public class GeneralSongListView extends JPanel {
 
+    // Iniciem els strings per quan es premi un botó
     public static final String BTN_BACK = "BTN_BACK";
     public static final String BTN_BUSCADOR = "BTN_BUSCADOR";
+
+    // Preparem els atributs
     private final Utils utils;
-    private HeaderView headerView;
-    private FooterView footerView;
+    private final HeaderView headerView;
+    private final FooterView footerView;
+
+    // Preparem els components de Swing
     private JTextField jBuscador;
     private JButton jCerca;
     private JTable table;
     private DefaultTableModel songsTableModel;
+
+    // Preparem l'array de Strings per a les cançons
     private static String[] columnHeaders = {"Title", "Artist", "Genre","Album","Uploaded by"};
 
+    /**
+     * Funció que servirà com a constructor de la StatisticsView
+     *
+     * @param headerView, vista per a posar el Header
+     * @param utils, per usar tots els seus mètodes
+     * @param footerView, vista per a posar el Footer
+     */
     public GeneralSongListView(Utils utils, HeaderView headerView, FooterView footerView){
         this.utils = utils;
         this.footerView = footerView;
@@ -35,73 +49,125 @@ public class GeneralSongListView extends JPanel {
         songsTableModel = new DefaultTableModel(columnHeaders, 0);
     }
 
+    /**
+     * Funció que servirà per a vincular amb el controller de GeneralSongListView
+     * i per a activar tots els seus listeners
+     *
+     * @param generalSongListViewController, controller en de la vista
+     */
     public void addGeneralSongListController(GeneralSongListViewController generalSongListViewController){
         jCerca.addActionListener(generalSongListViewController);
         table.addMouseListener(generalSongListViewController);
     }
 
+    /**
+     * Funció que servirà per a configurar tots els elements de Swing
+     * del GeneralSongListView
+     *
+     * No tindrà param ni return
+     */
     public void configureGeneralSonglistView() {
+        // Creem el BorderLayout, background i color gris
         setLayout(new BorderLayout());
         setBackground(Color.BLACK);
         Color gris = new Color(26,26,26);
 
-        //North
+        //NORTH
+        // Creem el JPanel i el configurem
         JPanel north = new JPanel();
         north.setBackground(Color.black);
+
+        // Afegim el JLabel amb el Header
         Icon logo = new ImageIcon(String.valueOf(AssetsFiles.MUSIC_LABEL));;
         north.add(headerView.configureHeader(logo));
         add(north, BorderLayout.NORTH);
 
-        //Center
+        //CENTER
+        // Creem el JPanel del center i el configurem
         JPanel center = new JPanel(new BorderLayout());
         center.setBackground(Color.BLACK);
         center.setBorder(BorderFactory.createEmptyBorder(0, 200, 80, 200));
 
-        //Buscador
+        // Afegim el JButton per al buscador
         Icon buscadorBtn = new ImageIcon(String.valueOf(AssetsFiles.BUSCADOR_BUTTON_IMG));
         jCerca = new JButton(buscadorBtn);
         jCerca.setActionCommand(BTN_BUSCADOR);
+
+        // Afegim el JTextField per a buscar la cançó
         jBuscador = new JTextField();
         center.add(utils.panelBuscador(jBuscador),BorderLayout.NORTH);
 
-        // Taula ListSong
+        // Afegim una JTable on es veuran totes les cançons
         table = new JTable(songsTableModel);
+
+        // Afegim un JScrollPane per fer scroll dins de la taula
         JScrollPane scrollpane = createSongListTable(table);
         center.add(scrollpane, BorderLayout.CENTER);
+
+        // Ho afegim tot dins del BorderLayout
         add(center, BorderLayout.CENTER);
 
+        // Creem la DefaultTableModel i la configurem
         TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<>(songsTableModel);
         sorter.setSortsOnUpdates(true);
         table.setRowSorter(sorter);
+
+        // Configurem el listener en cas que es premi el botó de buscar
         jBuscador.getDocument().addDocumentListener(new DocumentListener() {
+            /**
+             * Funció que servirà per a insertar la informació nova a la table
+             *
+             * @param e, update a insertar
+             */
             @Override
             public void insertUpdate(DocumentEvent e) {
                 search(jBuscador.getText(), sorter);
             }
 
+            /**
+             * Funció que servirà esborrar la informació prèviament buscada
+             *
+             * @param e, update a esborrar
+             */
             @Override
             public void removeUpdate(DocumentEvent e) {
                 search(jBuscador.getText(), sorter);
             }
 
+            /**
+             * Funció que servirà per a marcar la update que s'acaba d'afegir
+             *
+             * @param e, canvi realitzat
+             */
             @Override
             public void changedUpdate(DocumentEvent e) {
                 search(jBuscador.getText(), sorter);
             }
         });
 
+        // SOUTH
+        // Creem el JPanel del sud i el configurem
         JPanel south = new JPanel();
         south.setBackground(gris);
         south.setBorder(createEmptyBorder(2, 0, 2, 0));
 
+        // Afegim el Footer i ho afegim tot al BorderLayout
         south.add(footerView.configureFooter());
         add(south, BorderLayout.SOUTH);
 
     }
 
+    /**
+     * Funció que servirà com a constructor de la taula
+     * amb la qual es visualitzaran totes les cançons
+     *
+     * @param table, JTable amb la SongList creada
+     */
     public JScrollPane createSongListTable(JTable table){
+        // Creem el color gris
         Color gris = new Color(26,26,26);
 
+        // Configurem les mides i color de la taula
         table.setRowHeight(60);
         table.setGridColor(Color.gray);
         table.setBackground(gris);
@@ -110,28 +176,47 @@ public class GeneralSongListView extends JPanel {
         table.setSelectionBackground(table.getBackground());
         table.setSelectionForeground(Color.decode("#00DC00"));
 
+        // Configurem el header de la taula amb les seves mides i color
         DefaultTableCellRenderer header = new DefaultTableCellRenderer();
         header.setHorizontalAlignment(SwingConstants.LEFT);
         header.setForeground(Color.decode("#00DC00"));
         header.setFont(new Font("Gotham", Font.BOLD, 20));
         table.getTableHeader().setDefaultRenderer(header);
 
+        // Definim la font dels continguts de la taula
         table.setFont(new Font("Gotham", Font.BOLD, 20));
 
         return new JScrollPane(table);
     }
 
+    /**
+     * Funció per a retornar el valor introduït del buscador
+     *
+     * @return jBuscador, informació resultant
+     */
     public JTextField getjBuscador() {
         return jBuscador;
     }
 
+    /**
+     * Funció per a retornar el la JTable
+     *
+     * @return table, JTable a retornar
+     */
     public JTable getTable() {
         return table;
     }
 
+    /**
+     * Funció que servirà per a estructurar la informació
+     * de les cançons a la taula principal
+     *
+     * @param songs, array amb la informació de totes les songs
+     */
     public void fillTable(ArrayList<String> songs) {
         songsTableModel.setRowCount(0);
 
+        // Iniciem un bucle que recorri l'array i estructurem la informació
         for (String s : songs) {
             String[] songInfo = s.split("-");
             Object[] rowData = {songInfo[0], songInfo[1], songInfo[2], songInfo[3], songInfo[4]};
@@ -139,12 +224,21 @@ public class GeneralSongListView extends JPanel {
         }
     }
 
+    /**
+     * Funció que servirà per a buscar la cançó desitjada
+     *
+     * @param query, valor introduït de l'usuari
+     * @param sorter, informació per a canviar la taula
+     */
     private void search(String query, TableRowSorter<DefaultTableModel> sorter) {
 
+        // Ens assegurem que s'hagi introduït alguna cosa
         if (query.length() == 0) {
             sorter.setRowFilter(null);
-        } else {
-            sorter.setRowFilter(RowFilter.regexFilter("(?i)" + query, 0)); // Search by the first column (title)
+        }
+        // Busca per la primera columna, pel títol de la cançó
+        else {
+            sorter.setRowFilter(RowFilter.regexFilter("(?i)" + query, 0));
         }
     }
 }
--- src/Presentation/View/StatisticsView.java
+++ src/Presentation/View/StatisticsView.java
@@ -17,13 +17,23 @@ import static javax.swing.BorderFactory.createEmptyBorder;
 
 public class StatisticsView extends JPanel {
 
+    // Preparem els strings en cas que es premi el botó back
     public static final String BTN_BACK = "BTN_BACK";
+
+    // Preparem els atributs
     private final Utils utils;
     private final HeaderView headerView;
     private final FooterView footerView;
     private final SongManager songManager;
 
-
+    /**
+     * Funció que servirà com a constructor de la StatisticsView
+     *
+     * @param headerView, vista per a posar el Header
+     * @param utils, per usar tots els seus mètodes
+     * @param footerView, vista per a posar el Footer
+     * @param songManager, per a usar les estadístiques
+     */
     public StatisticsView(HeaderView headerView, Utils utils, FooterView footerView, SongManager songManager){
         this.headerView = headerView;
         this.footerView = footerView;
@@ -31,62 +41,101 @@ public class StatisticsView extends JPanel {
         this.songManager = songManager;
     }
 
+    /**
+     * Funció que servirà per a connectar amb el seu
+     * controller i activar els listeners
+     *
+     * @param statisticsViewController, controller de les Statistics
+     */
     public void addStatisticsController(StatisticsViewController statisticsViewController){
-        //set action command
         //jback.addActionListener(statisticsViewController);
     }
 
-
+    /**
+     * Funció que servirà per a configurar la StatisticsView
+     * i tots els seus components de Swing
+     *
+     * No tindrà ni param ni return
+     */
     public void configureStatisticsView() {
+        // Creem el BorderLayout
         setLayout(new BorderLayout());
+
         // NORTH
+        // Creem el JPanel del nord amb el FlowLayout
         JPanel north =  new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        // Configurem el JPanel i afegim el Label
         Icon statsImg = new ImageIcon(String.valueOf(AssetsFiles.STATISTICS_LABEL));
         north.add(headerView.configureHeader(statsImg));
         north.setBackground(Color.black);
         add(north, BorderLayout.NORTH);
 
         // CENTER
+        // Creem el JPanel del center amb el FlowLayout i el configurem
         JPanel center = new JPanel(new FlowLayout(FlowLayout.CENTER));
         center.setBackground(Color.BLACK);
+
+        // Creem un nou sub-JPanel que servirà per visualitzar les estadístiques
         JPanel graphPanel = new JPanel() {
+            /**
+             * Funció amb override que servirà per a dibuixar
+             * el gràfic dins del JPanel contínuament
+             *
+             * @param graphics, per recollir els gràfics que volem dibuixar
+             */
             @Override
             protected void paintComponent(Graphics graphics) {
+                // Preparem les variables i les eines
                 super.paintComponent(graphics);
                 Graphics2D graph2D = (Graphics2D) graphics;
 
+                // Agafem el mapa amb els gèneres i el seu nombre
                 Map<String, Integer> genreMap = songManager.createGenreMap();
 
-
+                // Agafem el gènere amb el màxim valor
                 int genreValue = Collections.max(genreMap.values());
+
+                // Calculem les mides de les barres
                 int barWidth = getWidth() / (genreMap.size() * 2);
                 int maxHeight = getHeight();
-                int x = 0;
 
+                // Iniciem un bucle que recorri tot el mapa
+                int x = 0;
                 for (Map.Entry<String, Integer> entry : genreMap.entrySet()) {
+                    // Agafem gènere i el seu valor
                     String genre = entry.getKey();
                     int count = entry.getValue();
 
+                    // Dibuixem l'alçada de la barra
+                    // Important recalcar: passem el count a double per a poder fer la operació
+                    // i un cop feta la operació, passem el resultat a int
                     int barHeight = (int) ((double) count / genreValue * maxHeight);
 
+                    // Configurem les mides i color de les barres
                     graph2D.setColor(Color.GREEN);
                     graph2D.fillRect(x, getHeight() - barHeight, barWidth, barHeight -25);
 
+                    // Configurem format dels Strings de cada gènere
                     graph2D.setColor(Color.WHITE);
                     graph2D.setFont(new Font("Gotham", Font.BOLD, 12));
                     graph2D.drawString(genre, x, getHeight() - 5);
 
+                    // Avancem la X per la següent barra
                     x += barWidth * 2;
                 }
             }
         };
+        // Configurem el gràfic del JPanel
         graphPanel.setPreferredSize(new Dimension(500, 300));
         graphPanel.setBackground(Color.BLACK);
         center.add(graphPanel);
 
+        // L'afegim al BorderLayout
         add(center, BorderLayout.CENTER);
 
         // SOUTH
+        // Creem el JPanel del sud, creant el color gris i configurant mides per al Footer
         Color gris = new Color(26,26,26);
         JPanel south = new JPanel();
         south.setBackground(gris);
