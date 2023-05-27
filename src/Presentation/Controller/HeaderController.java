2bb5ad3a0065a274d61f21c21b07b9371910f2b7
Raulincher <93159054+Raulincher@users.noreply.github.com>
2023-05-28 00:25:29 +0200

    comments in HeaderController

--- src/Presentation/Controller/HeaderController.java
+++ src/Presentation/Controller/HeaderController.java
@@ -19,7 +19,15 @@ public class HeaderController implements ActionListener {
     private MainView mainView;
     private FooterController footerController;
 
-
+    /**
+     * Funció que servirà per com a constructor del HeaderController
+     *
+     * @param headerView, utilitzarem els metodes dins de headerView
+     * @param userManager, utilitzarem el metodes dins de userManager, així tractarem tot el tema d'usuaris
+     * @param songManager, utilitzarem el metodes dins de songManager, així tractarem tot el tema de cançons
+     * @param playlistManager, utilitzarem el metodes dins de playlistManager, així tractarem tot el tema de playlists
+     * @param footerController, utilitzarem una funció dins d'aquest metode per així poder fer reset de la barra un cop sortim del nostre compte
+     */
     public HeaderController(HeaderView headerView, UserManager userManager, SongManager songManager, PlaylistManager playlistManager, FooterController footerController) {
         this.headerView = headerView;
         this.userManager = userManager;
@@ -27,17 +35,31 @@ public class HeaderController implements ActionListener {
         this.playlistManager = playlistManager;
         this.footerController = footerController;
     }
+    /**
+     * Funció que servirà per afegir la mainview dins del nostre headerController
+     *
+     * @param mainView, utilitzarem els metodes dins de mainview
 
+     */
     public void addMainView(MainView mainView){
         this.mainView = mainView;
     }
 
+
+    /**
+     * Funció que servirà per detectar si hem premut qualsevol botó de la vista del header
+     *
+     * No tindrà param ni return
+     */
+
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
+            //case back, que ens permetrá regresar a la vista del menu d'opcions
             case HeaderView.BTN_BACK:
                 mainView.showMainMenuCard();
                 break;
+            // case delete account, botó que ens permetrá borrar el compte actual
             case HeaderView.BTN_DELETEACC:
                 try {
                     int resposta = headerView.showPopUps("Are you sure?","Delete Account");
@@ -61,6 +83,7 @@ public class HeaderController implements ActionListener {
                     ex.printStackTrace();
                 }
                 break;
+            // case logout, botó que ens permetrà sortir del compte actual sense esborrar l'usuari
             case HeaderView.BTN_LOGOUT:
                 int resposta2 = headerView.showPopUps("Are you sure?","Log Out");
                 if (resposta2 == 0) {
